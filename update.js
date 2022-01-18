40e870ccd09fff4330da74a932e991b05ab7709e
aldy-windows <aldy.andika@gmail.com>
2022-01-18 11:13:26 +0700

    update src/data/config.js

--- update.js
+++ update.js
@@ -9,6 +9,8 @@ const token = 'YOUR_PERSONAL_ACCESS_TOKEN'
 const repoName = 'nqa-1'
 const filename = 'src/service-worker.js'
 const line = 24
+const filename2 = 'src/data/config.js'
+const line2 = 1
 
 const updates = () => {
   axios
@@ -27,16 +29,29 @@ const updates = () => {
           })
           data = data.join('\n')
           fs.writeFile(`target-repo/${filename}`, data, { encoding: 'utf-8' }, () => {
-            shell.cd(path.join('target-repo','api'))
-            shell.exec('npm install')
-            shell.exec('node index.js')
-            const target = path.join('..','src','db','output.js')
-            shell.cp('output.js', target)
-            shell.cd('..')
-            shell.exec('git config user.name nodeUpdate')
-            shell.exec('git config user.email tes@mail.com')
-            shell.exec(`git add ${path.join('src','*')} && git commit -m "latest cron update" && git push`)
-            shell.exec('git status')
+            fs.readFile(`target-repo/${filename2}`, { encoding: 'utf-8' }, (err2, data2) => {
+              if (!err2) {
+                data2 = data2.split('\n')
+                data2[line2 - 1] = data2[line2 - 1].replace(/\b([.0-9]+)\b/, (match, g1) => {
+                  const splitted2 = g1.split('.');
+                  splitted2[splitted2.length - 1] = parseInt(splitted2[splitted2.length - 1]) + 1;
+                  return splitted2.join('.')
+                })
+                data2 = data2.join('\n')
+                fs.writeFile(`target-repo/${filename2}`, data2, { encoding: 'utf-8' }, () => {
+                  shell.cd(path.join('target-repo','api'))
+                  shell.exec('npm install')
+                  shell.exec('node index.js')
+                  const target = path.join('..','src','db','output.js')
+                  shell.cp('output.js', target)
+                  shell.cd('..')
+                  shell.exec('git config user.name nodeUpdate')
+                  shell.exec('git config user.email tes@mail.com')
+                  shell.exec(`git add ${path.join('src','*')} && git commit -m "latest cron update" && git push`)
+                  shell.exec('git status')
+                })
+              }
+            })
           })
         } else {
           console.log(err)
